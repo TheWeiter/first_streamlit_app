34dc3454c346f0f21c624a05b0f597b740c3bf2a
TheWeiter <whyweiway@gmail.com>
2023-07-07 08:57:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 import requests
 import snowflake.connector
 from urllib.error import URLERrror
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
